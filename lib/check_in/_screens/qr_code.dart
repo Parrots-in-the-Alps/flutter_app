06e128991f072437f5a4d5a577693cc1d828af65
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-06-08 15:45:33 +0200

    qrcode OK

--- lib/check_in/_screens/qr_code.dart
+++ lib/check_in/_screens/qr_code.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:qrscan/qrscan.dart' as scanner;
 import 'package:flutter/services.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:vulcan_mobile_app/utils/app_bar.dart';
 
 class QRCode extends StatefulWidget {
@@ -11,33 +12,77 @@ class QRCode extends StatefulWidget {
 }
 
 class _QRCodeState extends State<QRCode> {
-  String result = "Hello World...!";
-  Future _scanQR() async {
-    try {
-      String? cameraScanResult = await scanner.scan();
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+  QRViewController? controller;
+  String result = "";
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
       setState(() {
-        result =
-            cameraScanResult!; // setting string result with cameraScanResult
+        result = scanData.code!;
       });
-    } on PlatformException catch (e) {
-      print(e);
-    }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const VulcanAppBar(title: "Scan Qr Code"),
-      body: Center(
-        child: Text(result), // Here the scanned result will be shown
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-          icon: const Icon(Icons.camera_alt),
-          onPressed: () {
-            _scanQR(); // calling a function when user click on button
-          },
-          label: const Text("Scan")),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+        appBar: const VulcanAppBar(title: "Scan Qr Code"),
+        body: Center(
+            child: Expanded(
+                flex: 4,
+                child: QRView(
+                  key: qrKey,
+                  onQRViewCreated: _onQRViewCreated,
+                ))));
   }
 }
+
+
+/**
+ * 
+ ,
+            Expanded(
+                flex: 1,
+                child: Center(
+                  child: Text(
+                    "Scan result: $result",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                )),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        if (result.isNotEmpty) {
+                          Clipboard.setData(ClipboardData(text: result));
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            content: Text("Copied to Clipboard"),
+                          ));
+                        }
+                      },
+                      child: const Text("Copy")),
+                  ElevatedButton(
+                      onPressed: () async {
+                        if (result.isNotEmpty) {
+                          final Uri url = Uri.parse(result);
+                          await launchUrl(url);
+                        }
+                      },
+                      child: const Text("Open"))
+                ],
+              ),
+            )
+ * 
+ */
