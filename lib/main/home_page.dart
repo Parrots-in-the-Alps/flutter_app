d7efaac55f3448046eeafce5bfa4c0423f18f840
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-06-08 14:24:52 +0200

    merge

--- lib/main/home_page.dart
+++ lib/main/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vulcan_mobile_app/check_in/_screens/qr_code.dart';
 import 'package:vulcan_mobile_app/utils/button.dart';
 import 'package:vulcan_mobile_app/write_nfc/_screens/pick_room_screen.dart';
 
@@ -19,8 +20,8 @@ class HomePage extends StatelessWidget {
             padding: EdgeInsets.all(24.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                MenuButton(text: 'Check-in', destination: PickRoomScreen()),
+              children: [
+                MenuButton(text: 'Check-in', destination: QRCode()),
                 MenuButton(
                     text: 'Ecrire une clé', destination: PickRoomScreen()),
                 MenuButton(text: 'Lire une clé', destination: PickRoomScreen()),
