d36061e14bd0ea32fd7415a0bcf03919bc6b30a0
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-06-09 14:12:59 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vulcan_mobile_app/main/home_page.dart';
 import 'package:vulcan_mobile_app/providers/ReservationsProvider.dart';
-import 'package:vulcan_mobile_app/simulation/_screens/simulation_failed.dart';
 
 void main() {
   runApp(
