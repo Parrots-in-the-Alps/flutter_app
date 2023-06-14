c6460725ef0020cb8393c8eba756dadabd4d6dca
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-06-14 10:49:08 +0200

    add SimulationProvider in MultiProvider main

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vulcan_mobile_app/main/home_page.dart';
 import 'package:vulcan_mobile_app/providers/reservation_provider.dart';
+import 'package:vulcan_mobile_app/providers/simulation_provider.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => ReservationProvider()),
+        ChangeNotifierProvider(create: (context) => SimulationProvider()),
       ],
       child: const MyApp(),
     ),
