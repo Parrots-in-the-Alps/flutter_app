1997623eda87b356e321f1c9e28ee1b172c5c29c
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-06-16 10:58:38 +0200

    fix simulation failed

--- lib/simulation/_screens/simulation_failed.dart
+++ lib/simulation/_screens/simulation_failed.dart
@@ -8,40 +8,35 @@ class SimulationFailed extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color(0xFF607D8B),
-        appBar: const VulcanAppBar(title: "Simulation Validated"),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              RawMaterialButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
-                },
-                elevation: 2.0,
-                fillColor: const Color(0xFFD32F2F),
-                padding: const EdgeInsets.all(30.0),
-                shape: const CircleBorder(),
-                child: const Icon(
-                  Icons.warning,
-                  size: 60.0,
-                  color: Colors.white,
-                ),
-              ),
-              const Text(
-                "Carte vide !\nAccès refusé !",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                ),
-              ),
-            ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        RawMaterialButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          },
+          elevation: 2.0,
+          fillColor: const Color(0xFFD32F2F),
+          padding: const EdgeInsets.all(30.0),
+          shape: const CircleBorder(),
+          child: const Icon(
+            Icons.warning,
+            size: 60.0,
+            color: Colors.white,
           ),
-        ));
+        ),
+        const Text(
+          "Carte vide !\nAccès refusé !",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
   }
 }
